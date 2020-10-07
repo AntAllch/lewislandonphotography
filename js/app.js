945f194046f00a45d70ec57de473ca185b66ac2d
AntAllch <anthonyallchorne@gmail.com>
2020-10-07 22:17:33 +0100

    Added modals to each image

--- js/app.js
+++ js/app.js
@@ -49,3 +49,30 @@ contactModal.addEventListener("click", (e) =>{
     }
 });
 
+
+//****************************************************//
+
+//Image modal
+
+const modal = document.querySelector(".modal");
+const previews = document.querySelectorAll(".portfolio-image-container img");
+const original = document.querySelector(".full-img");
+
+previews.forEach((preview) => {
+    preview.addEventListener("click", () => {
+        modal.classList.add("open");
+        original.classList.add("open");
+
+        //dynamic change
+        const originalSrc = preview.getAttribute("data-original");
+        original.src = `./img/${originalSrc}`;
+    })
+})
+
+modal.addEventListener("click", (e) => {
+    if(e.target.classList.contains("modal")){
+        modal.classList.remove("open");
+        original.classList.remove("open");
+    }
+})
+

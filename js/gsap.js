40b7572229a5112da00968c158d1f819bdd9d35c
AntAllch <anthonyallchorne@gmail.com>
2020-12-17 14:36:18 +0000

    Added GSAP animations

---
+++ js/gsap.js
@@ -0,0 +1,10 @@
+//****************************************************//
+
+//GSAP
+
+//Hero Images
+gsap.from(".hero-img", {opacity: 0, duration: 1, y: -30, stagger: 0.2})
+gsap.from("nav", {opacity: 0, duration: 2, delay: 1})
+
+//Most Recent
+// gsap.from(".most-recent-img", {opacity: 0, duration: 1, x: -30})
